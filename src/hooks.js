1697462d092c098939e816699988de106ae7a321
aprakasa <prakasa.arya@gmail.com>
2022-06-28 23:44:14 +0700

    chore: update sveltekit demo app

--- src/hooks.js
+++ src/hooks.js
@@ -1,25 +1,22 @@
-import cookie from 'cookie';
-import { v4 as uuid } from '@lukeed/uuid';
+import * as cookie from 'cookie';
 
-export const handle = async ({ request, resolve }) => {
-	const cookies = cookie.parse(request.headers.cookie || '');
-	request.locals.userid = cookies.userid || uuid();
+/** @type {import('@sveltejs/kit').Handle} */
+export const handle = async ({ event, resolve }) => {
+	const cookies = cookie.parse(event.request.headers.get('cookie') || '');
+	event.locals.userid = cookies['userid'] || crypto.randomUUID();
 
-	// TODO https://github.com/sveltejs/kit/issues/1046
-	const method = request.url.searchParams.get('_method');
-	if (method) {
-		request.method = method.toUpperCase();
-	}
+	const response = await resolve(event);
 
-	const response = await resolve(request);
-
-	if (!cookies.userid) {
+	if (!cookies['userid']) {
 		// if this is the first time the user has visited this app,
 		// set a cookie so that we recognise them when they return
-		response.headers['set-cookie'] = cookie.serialize('userid', request.locals.userid, {
-			path: '/',
-			httpOnly: true
-		});
+		response.headers.set(
+			'set-cookie',
+			cookie.serialize('userid', event.locals.userid, {
+				path: '/',
+				httpOnly: true
+			})
+		);
 	}
 
 	return response;
